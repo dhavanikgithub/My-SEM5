de27caa9ad24b59846e36a01652e61e8709bcee3
dhavanikgithub <110646988+dhavanikgithub@users.noreply.github.com>
2022-09-15 18:51:36 +0530

    Update p4_12.js

--- SP/Practical/Code/Practical 4/p4_12.js
+++ SP/Practical/Code/Practical 4/p4_12.js
@@ -1,5 +1,5 @@
 const path=require('path');
-var path_link="H:/BACKUP/node/myexamples/CIRCLE.js";
+var path_link="H:\\BACKUP\\node\\myexamples\\CIRCLE.js";
 console.log('Root: '+path.parse(path_link).root);
 console.log('Dir: '+path.parse(path_link).dir);
 console.log('BaseName: '+path.parse(path_link).base);
