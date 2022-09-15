16cb5fb732156abec715a3fe2b2f913d976adb33
dhavanikgithub <110646988+dhavanikgithub@users.noreply.github.com>
2022-09-15 09:42:42 +0530

    Add files via upload

---
+++ SP/Practical/Code/Practical 4/p4_2.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+//Sync Read
+console.log("*****Sync File Read*****");
+console.log(fs.readFileSync('p4_1.txt').toString());
+console.log("Read Complete\n");
+
+//Async Read
+console.log("*****Async File Read*****");
+fs.readFile('p4_1.txt',function(err,data){
+    if(err)
+    {
+        console.log(err);
+    }
+    else{
+        console.log(data.toString());
+    }
+});
+console.log("Read Complete");
+
