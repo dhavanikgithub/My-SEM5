d1b4b0b2f6954dd7558615f4d56862d956e3d02e
dhavanikgithub <110646988+dhavanikgithub@users.noreply.github.com>
2022-09-20 11:22:54 +0530

    Update bit_stuffing.py

--- CN/Practical/Code/Practical 4/bit_stuffing.py
+++ CN/Practical/Code/Practical 4/bit_stuffing.py
@@ -6,10 +6,11 @@ for i in range(0,len(data)):
     if(data[i]=='1'):
         f=1
         for j in range(1,6):
-            if(flag[j]!=data[i]):
+            if(flag[j]!=data[i] and i<len(data)):
                 f=0
                 break
             i=i+1
         if(f==1):
-            data.insert(i-1,'0')
+            if(data[i]!='0'):
+                data.insert(i,'0')
 print("Bit Stuff Data is: ","".join(data))
